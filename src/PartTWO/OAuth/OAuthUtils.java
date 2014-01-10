1de7157d90f9135d0c25d38d871eb56b8d27b8cc
dmanjuna <dmanjuna@asu.edu>
2014-01-10 02:18:45 -0700

    Removed confidentials credentials from OAuthUtils file

--- src/PartTWO/OAuth/OAuthUtils.java
+++ src/PartTWO/OAuth/OAuthUtils.java
@@ -5,36 +5,39 @@ public class OAuthUtils
 	public static final String REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token";
 	public static final String AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize";
 	public static final String ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token";
-		    
-    public static final String CONSUMER_SECRET1 = "67M9KppNTeu3XfdIaoE7dFrmCx1ap01YjasRVL7CnM";
-    public static final String CONSUMER_KEY1 = "vmQM5ftqraFK615x8tFBg";
+	
+	//please enter your consumer secret and consumer key of your application as registered on twitter below
+	//in these variables. We have used ten such applications to make the process of gathering data faster
+    public static final String CONSUMER_SECRET1 = "Enter Consumer Secret1 here";
+    public static final String CONSUMER_KEY1 = "Enter Consumer Key1 here";
    
-    public static final String CONSUMER_SECRET2 = "duodwYToScDwUFlXnUJ8764ZBlZ1QCXoArH8mr9iyY";
-    public static final String CONSUMER_KEY2 = "C0lTt4UtcaXOTgytahRA";
+    //Similarly fill in key-secret pairs below for all the applications you are going to use in this program.
+    public static final String CONSUMER_SECRET2 = " ";
+    public static final String CONSUMER_KEY2 = "";
  
-    public static final String CONSUMER_SECRET3 = "9zekGCcc7ByLBWqp42DblkMqX7LrLLqJa97s31Ifco8";
-    public static final String CONSUMER_KEY3 = "ORCTqMxSBVqj12qryMzY7A";
+    public static final String CONSUMER_SECRET3 = " ";
+    public static final String CONSUMER_KEY3 = " ";
   
-    public static final String CONSUMER_SECRET4 = "s6PgwYLS2QThBHaDkDlHas8JIMjV1fdFWizmMX0rk";
-    public static final String CONSUMER_KEY4 = "U5AwQjYPNvj7ukAdnFOXJQ";
+    public static final String CONSUMER_SECRET4 = " ";
+    public static final String CONSUMER_KEY4 = " ";
     
-    public static final String CONSUMER_SECRET5 = "lC7nvDd8Bu8Jp1A0isvOOaEOQ7GlxXlDbFlWZlUtICU";
-    public static final String CONSUMER_KEY5 = "dWumFWUaKYeVQKOyGO3Z0A";
+    public static final String CONSUMER_SECRET5 = " ";
+    public static final String CONSUMER_KEY5 = " ";
    
-    public static final String CONSUMER_SECRET6 = "zjlvGikKlwThfA5YsWQoTpX1OU6KL8pw9xeDUnGy6IM";
-    public static final String CONSUMER_KEY6 = "vcqDfzNG2XGZf4OA2NYQ";
+    public static final String CONSUMER_SECRET6 = " ";
+    public static final String CONSUMER_KEY6 = " ";
     
-    public static final String CONSUMER_SECRET7 = "1XwU0djyARq7VU33lTsNCMTKA9m6uY4zSNgVH7Hdew";
-    public static final String CONSUMER_KEY7 = "Fe3yKPiaKjLdgT4TUVcuaw";
+    public static final String CONSUMER_SECRET7 = " ";
+    public static final String CONSUMER_KEY7 = " ";
     
-    public static final String CONSUMER_SECRET8 = "EzUVghun0WhjoHjxvmbflxqTLSi8PBkVeGjW2fGv580";
-    public static final String CONSUMER_KEY8 = "LPC2eseSudZGgocWUmgxWw";
+    public static final String CONSUMER_SECRET8 = " ";
+    public static final String CONSUMER_KEY8 = " ";
     
-    public static final String CONSUMER_SECRET9 = "SNTrkX44ZuS2aDhWEkwTtiK5hiDzKFuv4qMkLVbh8";
-    public static final String CONSUMER_KEY9 = "2rlZCkcyBFJ3JcooKb0jQ";
+    public static final String CONSUMER_SECRET9 = " ";
+    public static final String CONSUMER_KEY9 = " ";
     
-    public static final String CONSUMER_SECRET10 = "6ZwZ5JYyd5UtkKjXqghWyKqJsOUcFCpWzItEyvLMxU";
-    public static final String CONSUMER_KEY10 = "ku3ftA8bbd0t2T1WD78pQ";
+    public static final String CONSUMER_SECRET10 = " ";
+    public static final String CONSUMER_KEY10 = " ";
   
    
 }
